bd5a1118dfe7ff24c278dbdc7341490c0ca4674c
zhanglingkang <lingkangzhangjava@163.com>
2014-09-09 11:45:17 +0800

    添加注释

--- code/demo/js/browser/node-percent-in-view.js
+++ code/demo/js/browser/node-percent-in-view.js
@@ -1,3 +1,9 @@
+/**
+ * @method nodePercentInView
+ * @description 一个节点在屏幕中显示的比例
+ * @param {Node} node 一个原生的dom节点
+ * @return {Number} 范围：0-1
+ */
 function nodePercentInView(node) {
     var nodeRect = node.getBoundingClientRect();
     var clientHeight = document.documentElement.clientHeight;//浏览器可见区域高度。
