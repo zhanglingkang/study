03e64ebea9614dd697a225bcc721f115d10ae10d
zlk <lingkangzhangjava@163.com>
2014-09-15 22:38:43 +0800

    update

--- code/js/browser/key-code.js
+++ code/js/browser/key-code.js
@@ -1,6 +1,12 @@
+"use strict";
+
 /**
  * 键盘事件的charCode在firefox下为0。keyCode在chrome和firefox下一致
  */
-var keyCode = {
-    ENTER: 13
-};
+
+define(function (require, exports, module) {
+    var keyCode = {
+        ENTER: 13
+    };
+    return keyCode;
+});
